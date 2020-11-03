1890e6969147abe0db0be7003adb874274e90eed
Bharath Karanth <devdaemon404@gmail.com>
2020-11-03 10:55:32 +0530

    Implement GET /api/employee/financial-docs

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const {
   postEmployeeInfo,
   getEmployeeInfo,
-  getFinancialDocs,
+  getFinancialDocsUrl,
+  getFinancialDocument,
 } = require('../controllers/employee');
 
 const { protect } = require('../middleware/auth');
 
 router.route('/').post(protect, postEmployeeInfo);
 router.route('/').get(protect, getEmployeeInfo);
-router.route('/financial-docs').post(protect, getFinancialDocs);
+router.route('/financial-docs').get(protect, getFinancialDocument);
+router.route('/financial-docs').post(protect, getFinancialDocsUrl);
 
 module.exports = router;
